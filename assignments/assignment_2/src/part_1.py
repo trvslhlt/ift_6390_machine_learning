ea26fdd698a77a67d64a4928bf02d3f80994be80
travis holt <trvslhlt@gmail.com>
2026-03-27 16:56:52 -0400

    refactor for next part 1 exps

--- assignments/assignment_2/src/part_1.py
+++ assignments/assignment_2/src/part_1.py
@@ -5,13 +5,39 @@ import utils
 
 import torch
 from torch import optim
+from dataclasses import dataclass
 
 
-def run_1_1(device: str, logs_dir: str, models_dir: str):
-    epochs = 500
-    learning_rate = 1e-3 # 0.001
-    momentum = 0.0
-    hidden_sizes = [40, 20] # 2 hidden layers
+ARTIFACT_PREFIX = 'exp_1'
+
+
+@dataclass
+class Output:
+    logs_dir: str
+    models_dir: str
+
+
+@dataclass
+class Hyperparams:
+    epochs: int
+    learning_rate: float
+    momentum: float
+
+    hidden_sizes: list[int]
+    activation_fn: Activation
+    dropout: float | None
+    batch_norm: bool
+
+
+@dataclass
+class RunConfig:
+    device: str
+    output: Output
+    hyperparams: Hyperparams
+
+
+def run_1(config: RunConfig):
+    hp = config.hyperparams
     
     training_dataloader, validation_dataloader, target_stats = get_smiles_dataloaders()
     feature_count = training_dataloader.dataset.tensors[0].shape[1]
@@ -20,9 +46,9 @@ def run_1_1(device: str, logs_dir: str, models_dir: str):
         'experiment': {
             'model': {},
             'hyperparams': {
-                'epochs': epochs,
-                'learning_rate': learning_rate,
-                'momentum': momentum,
+                'epochs': hp.epochs,
+                'learning_rate': hp.learning_rate,
+                'momentum': hp.momentum,
             },
             'target_stats': target_stats,
         }, 'epoch': [], 'batch': []}
@@ -30,7 +56,7 @@ def run_1_1(device: str, logs_dir: str, models_dir: str):
 
     model = MLP(
         input_size=feature_count,
-        hidden_sizes=hidden_sizes,
+        hidden_sizes=hp.hidden_sizes,
         output_size=1, # regression, single value prediction
         activation=Activation.RELU)
 
@@ -44,16 +70,16 @@ def run_1_1(device: str, logs_dir: str, models_dir: str):
 
     train(
         model=model,
-        epochs=epochs,
+        epochs=hp.epochs,
         training_dataloader=training_dataloader,
         validation_dataloader=validation_dataloader,
-        optimizer=optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum),
+        optimizer=optim.SGD(model.parameters(), lr=hp.learning_rate, momentum=hp.momentum),
         loss_fn=torch.nn.MSELoss(),
-        device=device,
+        device=config.device,
         on_batch_end=on_batch_end,
         on_epoch_end=on_epoch_end,
     )
 
-    artifact_id = 'exp_1_1'
-    utils.save_logs(logs, logs_dir, f'{artifact_id}__{utils.file_timestamp()}')
-    utils.save_model(model, models_dir, f'{artifact_id}__{utils.file_timestamp()}')
+    artifact_id = f'{ARTIFACT_PREFIX}_1'
+    utils.save_logs(logs, config.output.logs_dir, f'{artifact_id}__{utils.file_timestamp()}')
+    utils.save_model(model, config.output.models_dir, f'{artifact_id}__{utils.file_timestamp()}')
